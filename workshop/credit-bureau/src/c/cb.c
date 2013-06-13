33944e5d245cd82f05b22e73bc299e8cacf112e1
Carlos Argel Arias Cerda <argel.arias90@gmail.com>
2013-06-12 20:50:31 -0500

    avance de workshop codigo C

--- workshop/credit-bureau/src/c/cb.c
+++ workshop/credit-bureau/src/c/cb.c
@@ -1,8 +1,8 @@
 /**
- * Este aplicacion representa a una entidad crediticia la cual
- * concentra todos los creditos otorgados a los clientes de
- * diferentes entidades.
- */
+* Este aplicacion representa a una entidad crediticia la cual
+* concentra todos los creditos otorgados a los clientes de
+* diferentes entidades.
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -26,12 +26,17 @@ void doprocessing (int sock)
     if ((recvMsgSize = recv(sock, buffer, 256, 0)) < 0)
         perror("ERROR reading to socket1");
 
-    if(buffer[10] == '0')
-        searchrfc(buffer, sock);
+    if(buffer[3] == '|') //checks if add a credit to the Loan file
+        addcredit(buffer);
+    else
+        if(buffer[4] == '|') //checks if edit a credit from the Loan file
+            editcredit(buffer);
+        else
+            searchrfc(buffer, sock); //search for the credits based in the rfc
 
 
     /* Send received string and receive again until end of transmission */
-    while (recvMsgSize > 0)      /* zero indicates end of transmission */
+    while (recvMsgSize > 0) /* zero indicates end of transmission */
     {
         /* Echo message back to client */
         if (send(sock, buffer, recvMsgSize, 0) != recvMsgSize)
@@ -43,72 +48,121 @@ void doprocessing (int sock)
 
     }
 
-    //printf("%s\n", buffer);
-    closesocket(sock);    /* Close client socket */
+    closesocket(sock); /* Close client socket */
 }
 
 BOOL initW32()
 {
-		WSADATA wsaData;
-		WORD version;
-		int error;
+WSADATA wsaData;
+WORD version;
+int error;
 
-		version = MAKEWORD( 2, 0 );
+version = MAKEWORD( 2, 0 );
 
-		error = WSAStartup( version, &wsaData );
+error = WSAStartup( version, &wsaData );
 
-		/* check for error */
-		if ( error != 0 )
-		{
-		    /* error occured */
-		    return FALSE;
-		}
+/* check for error */
+if ( error != 0 )
+{
+/* error occured */
+return FALSE;
+}
 
-		/* check for correct version */
-		if ( LOBYTE( wsaData.wVersion ) != 2 ||
-		     HIBYTE( wsaData.wVersion ) != 0 )
-		{
-		    /* incorrect WinSock version */
-		    WSACleanup();
-		    return FALSE;
-		}
+/* check for correct version */
+if ( LOBYTE( wsaData.wVersion ) != 2 ||
+HIBYTE( wsaData.wVersion ) != 0 )
+{
+/* incorrect WinSock version */
+WSACleanup();
+return FALSE;
+}
 }
 
 searchrfc(char *rfc, int sock){
-    //printf("%s\n", rfc);
     FILE *fd;
-	int i, j, find=0, contline;
-	char caracter;
-	char line[150];
-	rfc[10] = '\0';
-	//char rfc[10];// = {'2','3','4','5','6','A','B','C','D','E'};
-	//printf("%s\n", rfc);
-	fd = fopen("file.txt", "r");
-	while(!feof(fd)){
-		for(i=0;i<100;i++){
-			line[i] = fgetc(fd);
-			if(line[i] == '\n' || feof(fd)){
-				break;
-				}
-		}
-		char fline[i];
-		for(contline=0;contline<i;contline++){
-			fline[contline] = line[contline];
-		}
+int i, j, contline;
+char caracter;
+char line[150];
+rfc[10] = '\0';
+fd = fopen("file.txt", "r");
+while(!feof(fd)){
+        //gets the length of the line
+for(i=0;i<100;i++){
+line[i] = fgetc(fd);
+if(line[i] == '\n' || feof(fd)){
+break;
+}
+}
+char fline[i];
+        //make a line without garbage
+for(contline=0;contline<i;contline++){
+fline[contline] = line[contline];
+}
 
+        //check if the rfc is in the line and send it to the server
         if(strstr(fline, rfc)){
             printf("%s\n", fline);
             send(sock,fline,i,0);
             send(sock,"@",1,0);
         }
-	}
-	fclose(fd);
+}
+fclose(fd);
+}
+
+addcredit(char *credit){
+    printf("add");
+    int i;
+    FILE *fd;
+    fd = fopen("file.txt", "a+");
+    fseek(fd,0,2); //go to the EOF to write there
+    fprintf(fd,"\n");
+    //erase from the line the 4 first chars "add|"
+    for(i=0;i<256;i++)
+        credit[i] = credit[i+4];
+    //erase the garbage from the line
+    for(i=0;i<256;i++){
+        if((credit[i] == '|' && credit[i+1] == 'Y' && credit[i+2] == 13) || (credit[i] == '|' && credit[i+1] == 'N' && credit[i+2] == 13))
+            //printf("%i", credit[i+2]);
+            credit[i+2] = '\0';
+    }
+    //write the credit in the file
+    fprintf(fd,credit);
+    fclose(fd);
+
+}
+
+editcredit(char *credit){
+    printf("edit");
+    FILE *fd;
+    fd = fopen("file.txt", "a+");
+    int i;
+    char line[256];
+    //erase from the line the 5 first chars "edit|"
+    for(i=0;i<256;i++)
+        credit[i] = credit[i+5];
+    //erase the garbage from the line
+    for(i=0;i<256;i++){
+        if((credit[i] == '|' && credit[i+1] == 'Y' && credit[i+2] == 13) || (credit[i] == '|' && credit[i+1] == 'N' && credit[i+2] == 13))
+            credit[i+2] = '\0';
+    }
+    //search to the credit ti edit *still on build*
+    while(!feof(fd)){
+        fgets(line,256,fd);
+        if(strstr(line, credit)){
+            fseek(fd,-1,1);
+            fputc('N',fd);
+            //printf("%c\n",fgetc(fd));
+            fseek(fd,-1,1);
+        }
+
+    }
+    fclose(fd);
 }
 
 int main()
 {
 
-	 initW32(); /* Necesaria para compilar en Windows */
+initW32(); /* Necesaria para compilar en Windows */
 
    int fd, fd2; /* los descriptores de archivos */
 
@@ -146,7 +200,7 @@ int main()
       exit(-1);
    }
 
-   if(listen(fd,BACKLOG) == -1) {  /* llamada a listen() */
+   if(listen(fd,BACKLOG) == -1) { /* llamada a listen() */
       printf("error en listen()\n");
       exit(-1);
    }
@@ -170,5 +224,6 @@ int main()
 
    } /* end while */
 }
+
 
 
